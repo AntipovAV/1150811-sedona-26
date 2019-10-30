d3ef112858709c0fbfd9a52b2aca92786e77bd88
AntipovAV <antipov.av7.1@gmail.com>
2019-10-30 17:04:05 +0500

    Финальный вариант

--- js/script.js
+++ js/script.js
@@ -1,32 +1,36 @@
 var link = document.querySelector(".modal-link");
-var popup = document.querySelector(".modal");
+var modal = document.querySelector(".modal-searching");
 
-var form = popup.querySelector("form");
-var arrival = popup.querySelector("[name=arrival]");
-var departure = popup.querySelector("[name=departure]");
-var adults = popup.querySelector("[name=adults]");
-var children = popup.querySelector("[name=children]");
+var form = modal.querySelector("form");
+var arrival = modal.querySelector("[name=arrival]");
+var departure = modal.querySelector("[name=departure]");
+var adults = modal.querySelector("[name=adults]");
+var children = modal.querySelector("[name=children]");
 
 var isStorageSupport = true;
 var storage = "";
 
-try {
-  storage = localStorage.getItem("login");
-} catch (err) {
-  isStorageSupport = false;
+if (modal) {
+  modal.classList.add("modal-close");
 }
 
 link.addEventListener("click", function (evt) {
   evt.preventDefault();
-  popup.classList.add("modal-show");
+
+  if (modal.classList.contains("modal-close")) {
+    modal.classList.remove("modal-close");
+    modal.classList.add("modal-show");}
+    else {
+    modal.classList.add("modal-close");
+    }
 });
 
 form.addEventListener("submit", function (evt) {
   if (!arrival.value || !departure.value || !adults.value || !children.value) {
   evt.preventDefault();
-  popup.classList.remove("modal-error");
-  popup.offsetWidth = popup.offsetWidth;
-  popup.classList.add("modal-error");
+  modal.classList.remove("modal-error");
+  modal.offsetWidth = modal.offsetWidth;
+  modal.classList.add("modal-error");
 } else {
   if (isStorageSupport) {
       localStorage.setItem("adults", adults.value);
@@ -38,9 +42,9 @@ form.addEventListener("submit", function (evt) {
 window.addEventListener("keydown", function (evt) {
   if (evt.keyCode === 27) {
     evt.preventDefault();
-    if (popup.classList.contains("modal-show")) {
-      popup.classList.remove("modal-show");
-      popup.classList.remove("modal-error");
+    if (modal.classList.contains("modal-show")) {
+      modal.classList.add("modal-close");
+      modal.classList.remove("modal-error");
     }
   }
 });
